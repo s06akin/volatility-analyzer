0ab54aaa640021f7749423934595f0470e1901d2
s06akin <sobakin.sun@ya.ru>
2023-01-04 12:32:39 +0300

    logging

--- volatility-analyzer.py
+++ volatility-analyzer.py
@@ -614,6 +614,16 @@ while True:
 
                     break
 
+    print('*' * 60)
+    print(mf_list)
+    print('-' * 60)
+    s = [[str(e) for e in row] for row in bots]
+    lens = [max(map(len, col)) for col in zip(*s)]
+    fmt = '\t'.join('{{:{}}}'.format(x) for x in lens)
+    table = [fmt.format(*row) for row in s]
+    print('\n'.join(table))
+    print('*' * 60)
+
     cmc_req_cnt += 1
 
     start = time.perf_counter()
